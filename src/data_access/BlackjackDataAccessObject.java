e4c5daca0d5a7c691f39ee2df8ace68d61a0ed5d
lklkevin <88564778+lklkevin@users.noreply.github.com>
2023-10-30 19:23:34 -0400

    wrong files

--- src/data_access/BlackjackDataAccessObject.java
+++
@@ -1,6 +0,0 @@
-package data_access;
-
-import use_case.blackjack.BlackjackDataAccessInterface;
-
-public class BlackjackDataAccessObject implements BlackjackDataAccessInterface {
-}
--- src/use_case/blackjack/BlackjackDataAccessInterface.java
+++
@@ -1,4 +0,0 @@
-package use_case.blackjack;
-
-public interface BlackjackDataAccessInterface {
-}
