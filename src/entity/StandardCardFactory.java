88570aa2c303ebe9e07c184a0013668ffe56739d
likevi53 <lkevin.li@mail.utoronto.ca>
2023-11-10 23:04:19 -0500

    fixed error in getting the correct index

--- src/entity/StandardCardFactory.java
+++ src/entity/StandardCardFactory.java
@@ -7,8 +7,8 @@ public class StandardCardFactory implements CardFactory{
     public StandardCardFactory(){}
     @Override
     public Card create(JSONArray input, int index) {
-        String value = input.getJSONObject(0).getString("value");
-        String img = input.getJSONObject(0).getString("image");
+        String value = input.getJSONObject(index).getString("value");
+        String img = input.getJSONObject(index).getString("image");
         return new StandardCard(value, img);
     }
 }
