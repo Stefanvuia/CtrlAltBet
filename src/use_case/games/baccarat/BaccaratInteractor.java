5eae330e0ce635235fb88d0f6fc8ac7850803f20
lklkevin <88564778+lklkevin@users.noreply.github.com>
2023-12-03 21:28:17 -0500

    implemented facade design pattern inside baccarat interactor to better adhere to srp

--- src/use_case/games/baccarat/BaccaratInteractor.java
+++ src/use_case/games/baccarat/BaccaratInteractor.java
@@ -12,9 +12,10 @@ import java.util.Map;
 import java.util.Set;
 
 public class BaccaratInteractor implements BaccaratInputBoundary {
-    final CardsAPIInterface cardsAPI;
 
-    final GameDataAccessInterface gameDAO;
+    final BaccaratDataAccess baccaratDataAccess;
+
+    final BaccaratGameLogic baccaratGameLogic;
 
     final BaccaratOutputBoundary baccaratPresenter;
 
@@ -24,8 +25,8 @@ public class BaccaratInteractor implements BaccaratInputBoundary {
                               GameDataAccessInterface gameDAO,
                               BaccaratOutputBoundary baccaratPresenter,
                               HistoryDataAccessInterface historyDAO) {
-        this.cardsAPI = cardsAPI;
-        this.gameDAO = gameDAO;
+        this.baccaratGameLogic = new BaccaratGameLogic(cardsAPI);
+        this.baccaratDataAccess = new BaccaratDataAccess(historyDAO, gameDAO);
         this.baccaratPresenter = baccaratPresenter;
         this.historyDAO = historyDAO;
     }
@@ -40,80 +41,22 @@ public class BaccaratInteractor implements BaccaratInputBoundary {
             sum += bet.get(key);
         }
 
-        if (checkBet(username, sum)) {
-            Player banker = new BaccaratPlayer();
-            Player player = new BaccaratPlayer();
-            String deckId = cardsAPI.shuffleNew(8);
-
-            BaccaratGameInterface game = new BaccaratGame(player, banker, deckId);
-            game.addToHand(player, cardsAPI.draw(deckId, 2));
-            game.addToHand(banker, cardsAPI.draw(deckId, 2));
-
-            String winner = thirdCardHelper(game, player, banker);
-            int payout = payoutHelper(username, bet, winner, sum);
+        if (baccaratDataAccess.checkBet(username, sum)) {
+            baccaratGameLogic.startGame();
+            String winner = baccaratGameLogic.thirdCardHelper();
+            int payout = baccaratDataAccess.payoutHelper(bet, winner, sum);
 
             BaccaratOutputData outputData = new BaccaratOutputData(
                     winner.toUpperCase() + " win. Your payout is " + payout + "!",
-                    gameDAO.getFund(username),
-                    player.getHand(),
-                    banker.getHand());
+                    baccaratDataAccess.getFund(),
+                    baccaratGameLogic.getPlayerHand(),
+                    baccaratGameLogic.getBankerHand());
             baccaratPresenter.preparePayoutView(outputData);
         } else {
-            BaccaratOutputData outputData = new BaccaratOutputData("Insufficient funds!", gameDAO.getFund(username));
+            BaccaratOutputData outputData = new BaccaratOutputData(
+                    "Insufficient funds!",
+                    baccaratDataAccess.getFund());
             baccaratPresenter.prepareFailView(outputData);
         }
-    }
-
-    private boolean checkBet(String username, int betSum) {
-        if (gameDAO.getFund(username) >= betSum) {
-            gameDAO.editFund(username, -betSum);
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    private String thirdCardHelper(BaccaratGameInterface game, Player player, Player banker) {
-        if (game.sumHand(player) <= 5) {
-            game.addToHand(player, cardsAPI.draw(game.getDeck()));
-
-            if (game.sumHand(banker) <= 2) {
-                game.addToHand(banker, cardsAPI.draw(game.getDeck()));
-            } else if (game.sumHand(banker) <= 3 && game.sumHand(player) != 8) {
-                game.addToHand(banker, cardsAPI.draw(game.getDeck()));
-            } else if (game.sumHand(banker) <= 4 && Set.of(2, 3, 4, 5, 6, 7).contains(game.sumHand(player))) {
-                game.addToHand(banker, cardsAPI.draw(game.getDeck()));
-            } else if (game.sumHand(banker) <= 5 && Set.of(4, 5, 6, 7).contains(game.sumHand(player))) {
-                game.addToHand(banker, cardsAPI.draw(game.getDeck()));
-            } else if (game.sumHand(banker) <= 6 && Set.of(6, 7).contains(game.sumHand(player))) {
-                game.addToHand(banker, cardsAPI.draw(game.getDeck()));
-            }
-        } else if (game.sumHand(banker) <= 5) {
-            game.addToHand(banker, cardsAPI.draw(game.getDeck()));
-        }
-
-        if (game.sumHand(player) > game.sumHand(banker)) {
-            return "player";
-        } else if (game.sumHand(player) < game.sumHand(banker)) {
-            return "banker";
-        } else {
-            return "tie";
-        }
-    }
-
-    private int payoutHelper(String username, Map<String, Integer> bet, String winner, int betSum) {
-        int payout;
-        if (winner.equals("tie")) {
-            payout = bet.get(winner) * 8;
-        } else if (winner.equals("banker")) {
-            payout = (int) (bet.get(winner) * 0.95);
-        } else {
-            payout = bet.get(winner);
-        }
-        gameDAO.editFund(username, payout + bet.get(winner));
-
-        payout = payout + bet.get(winner) - betSum;
-        historyDAO.addPayout(username, "baccarat", payout);
-        return payout;
     }
 }
