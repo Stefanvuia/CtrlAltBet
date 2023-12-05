251eda64c0513a30e2cc83fe61e88f29cd571974
Zixuan Lu <zixuan.lu@mail.utoronto.ca>
2023-12-04 20:30:32 -0500

    write javadocs for AccountInputData class

--- src/use_case/game_menu/account/AccountInputData.java
+++ src/use_case/game_menu/account/AccountInputData.java
@@ -1,12 +1,29 @@
 package use_case.game_menu.account;
 
+/**
+ * Represents input data for account-related operations in the game menu.
+ */
 public class AccountInputData {
+
+    /**
+     * The username associated with the account.
+     */
     private final String username;
 
+    /**
+     * Constructs a new AccountInputData with the specified username.
+     *
+     * @param username The username associated with the account.
+     */
     public AccountInputData(String username) {
         this.username = username;
     }
 
+    /**
+     * Gets the username associated with the account.
+     *
+     * @return The username.
+     */
     public String getUsername() {
         return this.username;
     }
