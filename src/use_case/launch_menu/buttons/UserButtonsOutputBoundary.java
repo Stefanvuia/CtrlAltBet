5cfca65182b0e4a4238e034fa69a153ce8b69907
kathyxu1 <yanran.xu@mail.utoronto.ca>
2023-12-04 21:50:58 -0500

    added javadocs for buttons use case

--- src/use_case/launch_menu/buttons/UserButtonsOutputBoundary.java
+++ src/use_case/launch_menu/buttons/UserButtonsOutputBoundary.java
@@ -1,5 +1,22 @@
 package use_case.launch_menu.buttons;
 
+/**
+ * The {@code UserButtonsOutputBoundary} interface defines the contract for presenting the output
+ * of user button interactions in the launch menu.
+ */
 public interface UserButtonsOutputBoundary {
+
+    /**
+     * Prepares to swap or present the output based on the provided {@code outputData}.
+     *
+     * <p>Implementations of this method should handle the presentation of the output, which may involve
+     * swapping to a new view or performing other relevant actions based on the user's button interaction.</p>
+     *
+     * @param outputData The output data containing information to be presented or used for swapping views.
+     */
     void prepareSwap(UserButtonsOutputData outputData);
 }
+
+
+
+
