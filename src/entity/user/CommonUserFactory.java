b3baa8737c8713c40088952d62a2723c846bd786
Zixuan Lu <zixuan.lu@mail.utoronto.ca>
2023-12-04 20:11:29 -0500

    write javadocs for CommonUserFactory class

--- src/entity/user/CommonUserFactory.java
+++ src/entity/user/CommonUserFactory.java
@@ -2,7 +2,16 @@ package entity.user;
 
 import java.time.LocalDateTime;
 
+/**
+ * Factory class responsible for creating instances of {@link CommonUser}.
+ */
 public class CommonUserFactory implements UserFactory {
+
+    /**
+     * Creates a new instance of {@link CommonUser}.
+     *
+     * @return A new {@link CommonUser} instance.
+     */
     @Override
     public User create(String name, String password, LocalDateTime ltd, int balance) {
         return new CommonUser(name, password, ltd, balance);
