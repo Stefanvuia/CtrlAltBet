fb340ea08a73364ff97949c4d504fd3d8e7f4c04
lklkevin <88564778+lklkevin@users.noreply.github.com>
2023-12-05 02:12:22 -0500

    added javadocs for launch game buttons

--- src/use_case/game_menu/launch_game/LaunchInputData.java
+++ src/use_case/game_menu/launch_game/LaunchInputData.java
@@ -1,19 +1,45 @@
 package use_case.game_menu.launch_game;
 
+/**
+ * Represents input data for launching a game from the game menu.
+ */
 public class LaunchInputData {
+
+    /**
+     * The username of the player initiating the game launch.
+     */
     private final String username;
 
+    /**
+     * The selected game to be launched.
+     */
     private final String game;
 
+    /**
+     * Constructs a LaunchInputData object with the specified username and selected game.
+     *
+     * @param username The username of the player initiating the game launch.
+     * @param game     The selected game to be launched.
+     */
     public LaunchInputData(String username, String game) {
         this.username = username;
         this.game = game;
     }
 
+    /**
+     * Retrieves the username of the player initiating the game launch.
+     *
+     * @return The username of the player.
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Retrieves the selected game to be launched.
+     *
+     * @return The selected game.
+     */
     public String getGame() {
         return game;
     }
--- src/use_case/games/blackjack/blackjack_logic/BlackJackStandInteractor.java
+++ src/use_case/games/blackjack/blackjack_logic/BlackJackStandInteractor.java
@@ -59,6 +59,39 @@ public class BlackJackStandInteractor implements BlackJackStandInputBoundary {
      */
     @Override
     public void execute(BlackJackInputGameData blackJackInputGameData) {
-        // ... (Method logic is already commented in the code)
+        BlackJackGameInterface blackJackGameInterface = blackJackInputGameData.getGame();
+        Player dealer = blackJackGameInterface.getDealer();
+        BlackJackPlayer user = (BlackJackPlayer) blackJackGameInterface.getPlayer();
+        String deck = blackJackGameInterface.getDeck();
+        Integer playerSum = blackJackGameInterface.sumHand(user);
+
+        int dealerSum = blackJackGameInterface.sumHand(dealer);
+        int bet = user.getBet();
+
+        while (dealerSum < 17) {
+            blackJackGameInterface.addToHand(dealer, cardsAPI.draw(deck));
+            dealerSum = blackJackGameInterface.sumHand(dealer);
+        }
+
+        if (blackJackGameInterface.userWin() && Integer.valueOf(user.getHand().size()).equals(2) && playerSum.equals(21)) {
+            // Player blackjack
+            dataAccessObject.editFund(user.getUsername(), (int) (bet * 2.5));
+            historyDAO.addPayout(user.getUsername(), "blackjack", (int) (bet * 1.5));
+            blackJackStandPresenter.prepareWinView(new BlackJackOutputGameData(blackJackGameInterface, true, (int) (user.getBet() * 1.5)));
+        } else if (blackJackGameInterface.userWin()) {
+            // Player win
+            dataAccessObject.editFund(user.getUsername(), (user.getBet() * 2));
+            historyDAO.addPayout(user.getUsername(), "blackjack", bet);
+            blackJackStandPresenter.prepareWinView(new BlackJackOutputGameData(blackJackGameInterface, true, user.getBet()));
+        } else if (playerSum.equals(dealerSum)) {
+            // Push
+            dataAccessObject.editFund(user.getUsername(), user.getBet());
+            historyDAO.addPayout(user.getUsername(), "blackjack", 0);
+            blackJackStandPresenter.preparePushView(new BlackJackOutputGameData(blackJackGameInterface, true, 0));
+        } else {
+            // Dealer win
+            historyDAO.addPayout(user.getUsername(), "blackjack", -bet);
+            blackJackStandPresenter.prepareLoseView(new BlackJackOutputGameData(blackJackGameInterface, true, -user.getBet()));
+        }
     }
 }
