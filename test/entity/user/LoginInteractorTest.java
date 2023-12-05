0c50940b17ec67c0fdb5167de3c41cd46932e1f4
kathyxu1 <yanran.xu@mail.utoronto.ca>
2023-12-04 21:27:17 -0500

    added test for signup and login use case, added javadoc for both test classes

--- test/entity/user/LoginInteractorTest.java
+++ test/entity/user/LoginInteractorTest.java
@@ -1,2 +1,159 @@
-package entity.user;public class LoginInteractorTest {
+package entity.user;
+import org.junit.jupiter.api.Test;
+import use_case.launch_menu.login.*;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+/**
+ * Unit tests for the LoginInteractor class.
+ */
+public class LoginInteractorTest {
+
+    /**
+     * Test for successful user login.
+     */
+    @Test
+    public void testLoginUserSuccess() {
+        // Arrange
+        LoginUserDataAccessInterface dataAccess = new TestLoginUserDataAccess(true, true);
+        TestLoginOutputBoundary presenter = new TestLoginOutputBoundary();
+
+        LoginInteractor interactor = new LoginInteractor(dataAccess, presenter);
+
+        LoginInputData inputData = new LoginInputData("validUser", "validPassword");
+
+        // Act
+        interactor.loginUser(inputData);
+
+        // Assert
+        assertTrue(presenter.isSuccessViewCalled());
+    }
+
+    /**
+     * Test for attempting to log in with a nonexistent user.
+     */
+
+    @Test
+    public void testLoginUserUserNotExist() {
+        // Arrange
+        LoginUserDataAccessInterface dataAccess = new TestLoginUserDataAccess(false, false);
+        TestLoginOutputBoundary presenter = new TestLoginOutputBoundary();
+
+        LoginInteractor interactor = new LoginInteractor(dataAccess, presenter);
+
+        LoginInputData inputData = new LoginInputData("nonexistentUser", "password");
+
+        // Act
+        interactor.loginUser(inputData);
+
+        // Assert
+        assertTrue(presenter.isFailViewCalled());
+        assertEquals("User does not exist", presenter.getErrorMessage());
+    }
+
+    /**
+     * Test for attempting to login with an incorrect password.
+     */
+
+    @Test
+    public void testLoginUserIncorrectPassword() {
+        // Arrange
+        LoginUserDataAccessInterface dataAccess = new TestLoginUserDataAccess(true, false);
+        TestLoginOutputBoundary presenter = new TestLoginOutputBoundary();
+
+        LoginInteractor interactor = new LoginInteractor(dataAccess, presenter);
+
+        LoginInputData inputData = new LoginInputData("validUser", "invalidPassword");
+
+        // Act
+        interactor.loginUser(inputData);
+
+        // Assert
+        assertTrue(presenter.isFailViewCalled());
+        assertEquals("User password is Incorrect. Please try again", presenter.getErrorMessage());
+    }
+
+    /**
+     * Test for creating a common user using the CommonUserFactory.
+     */
+
+    @Test
+    public void testCreateCommonUser() {
+        // Arrange
+        UserFactory userFactory = new CommonUserFactory();
+
+        // Act
+        User user = userFactory.create("testUser", "testPassword", LocalDateTime.now(), 0);
+
+        // Assert
+        assertNotNull(user);
+        assertEquals("testUser", user.getName());
+        assertEquals("testPassword", user.getPassword());
+        assertEquals(0, user.getBalance());
+    }
+}
+
+/**
+ * Fake implementation of LoginUserDataAccessInterface for testing purposes.
+ */
+class TestLoginUserDataAccess implements LoginUserDataAccessInterface {
+
+    private final boolean userExists;
+    private final boolean passwordIsValid;
+
+    public TestLoginUserDataAccess(boolean userExists, boolean passwordIsValid) {
+        this.userExists = userExists;
+        this.passwordIsValid = passwordIsValid;
+    }
+
+    @Override
+    public boolean existsByName(String identifier) {
+        return userExists;
+    }
+
+    @Override
+    public User getUserByName(String username) {
+        return new CommonUserFactory().create("testUser", "testPassword", LocalDateTime.now(), 0);
+    }
+
+    @Override
+    public boolean validatePassword(String identifier, String password) {
+        return passwordIsValid;
+    }
+}
+
+/**
+ * Fake implementation of LoginOutputBoundary for testing purposes.
+ */
+
+
+class TestLoginOutputBoundary implements LoginOutputBoundary {
+
+    private boolean successViewCalled = false;
+    private boolean failViewCalled = false;
+    private String errorMessage;
+
+    @Override
+    public void prepareSuccessView(LoginOutputData user) {
+        successViewCalled = true;
+    }
+
+    @Override
+    public void prepareFailView(UserDataAccessFailed error) {
+        failViewCalled = true;
+        errorMessage = error.getMessage();
+    }
+
+    public boolean isSuccessViewCalled() {
+        return successViewCalled;
+    }
+
+    public boolean isFailViewCalled() {
+        return failViewCalled;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
 }
