018c888a20906c42a0ff4b567f65f79867f4f9f0
Zixuan Lu <zixuan.lu@mail.utoronto.ca>
2023-12-04 20:28:34 -0500

    write javadocs for UpdateOutputData class

--- src/use_case/account_menu/update/UpdateOutputData.java
+++ src/use_case/account_menu/update/UpdateOutputData.java
@@ -3,15 +3,30 @@ package use_case.account_menu.update;
 
 import entity.user.User;
 
-// Sign up response body
+/**
+ * Represents the response body for a successful user balance update operation.
+ */
 public class UpdateOutputData {
 
+    /**
+     * The new funds after the balance update.
+     */
     private final int newFunds;
 
+    /**
+     * Constructs a new UpdateOutputData with the specified new funds value.
+     *
+     * @param newFunds The new funds after the balance update.
+     */
     public UpdateOutputData(int newFunds) {
         this.newFunds = newFunds;
     }
 
+    /**
+     * Gets the new funds value after the balance update.
+     *
+     * @return The new funds value.
+     */
     public int getFunds() {
         return newFunds;
     }
