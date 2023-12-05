a334e026b2eeb1de69b2f7d10fdfa8ae95e6bf9d
Zixuan Lu <zixuan.lu@mail.utoronto.ca>
2023-12-04 20:51:46 -0500

    write javadocs for SignOutInteractor class

--- src/use_case/account_menu/sign_out/SignOutInteractor.java
+++ src/use_case/account_menu/sign_out/SignOutInteractor.java
@@ -1,12 +1,28 @@
 package use_case.account_menu.sign_out;
 
+/**
+ * Represents the interactor for handling sign-out operations in the account menu.
+ * Implements the SignOutInputBoundary interface.
+ */
 public class SignOutInteractor implements SignOutInputBoundary {
+
+    /**
+     * The presenter responsible for handling sign-out-related output.
+     */
     private final SignOutOutputBoundary signOutPresenter;
 
+    /**
+     * Constructs a new SignOutInteractor with the specified sign-out presenter.
+     *
+     * @param signOutPresenter The presenter responsible for handling sign-out-related output.
+     */
     public SignOutInteractor(SignOutOutputBoundary signOutPresenter) {
         this.signOutPresenter = signOutPresenter;
     }
 
+    /**
+     * Executes the sign-out operation by preparing the view through the sign-out presenter.
+     */
     @Override
     public void execute() {
         signOutPresenter.prepareWelcomeView();
